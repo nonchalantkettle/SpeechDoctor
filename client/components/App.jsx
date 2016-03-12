7590ab04e5f6e66b839764c6e339139daa38c6da
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-12 15:09:26 -0800

    Add app.jsx, fix linter for client and server

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, IndexRoute, browserHistory, hashHistory } from 'react-router';
+// import { Router, Route, Link, IndexRoute, browserHistory, hashHistory } from 'react-router';
 
 import Nav from './Nav';
 
@@ -8,18 +8,25 @@ import Nav from './Nav';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      showText: false,
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      showText: true,
+    });
   }
 
   render() {
-    return(
+    return (
       <Nav />
-    )
+    );
   }
-
 }
 
-ReactDOM.render (
+ReactDOM.render(
   <App />,
-  document.getElementById('app');
+  document.getElementById('app')
 );
