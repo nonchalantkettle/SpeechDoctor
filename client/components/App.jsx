7a4d3b95544286355000bb8e66c671501338271d
Benny Hung <bennyhungry@gmail.com>
2016-03-15 14:19:11 -0700

    Pass down handleLandingBtnClick method to LandingPage, which then passes it down to its LandingButtons

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
 import TextView from './TextView.jsx';
 import SpeechView from './SpeechView.jsx';
 import Nav from './Nav.jsx';
@@ -10,24 +11,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // showText: false,
       onLandingPage: true,
     };
   }
 
-  handleClick() {
+  handleLandingBtnClick = () => {
     this.setState({
-      onLandingPage: false,
+      onLandingPage: !this.state.onLandingPage,
+    }, () => {
+      console.log(this.state.onLandingPage);
     });
   }
 
   render() {
+    const Children = React.cloneElement(this.props.children, {
+      state: this.state,
+      handleLandingBtnClick: this.handleLandingBtnClick,
+    });
     return (
       <div>
         <Nav onLandingPage={this.state.onLandingPage} />
-        {this.props.children}
+        {Children}
       </div>
     );
+  }
+}
+
+class LandingPageHandler extends React.Component {
+  render() {
+    console.log('app props handlebuttonclick passed ', this.props.handleLandingBtnClick);
+    return (
+      <LandingPage handleLandingBtnClick={this.props.handleLandingBtnClick} />
+    )
   }
 }
 
@@ -36,7 +51,7 @@ const app = document.getElementById('app');
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={LandingPage} />
+      <IndexRoute component={LandingPageHandler} />
       <Route path="/speech" component={SpeechView} />
       <Route path="/text" component={TextView} />
     </Route>
