fc5906f53bda276a538234cb1bf8ba3233bc00a8
Benny Hung <bennyhungry@gmail.com>
2016-03-15 14:21:25 -0700

    Clean up code and spacing

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -18,14 +18,14 @@ export default class Nav extends React.Component {
   render() {
     return (
       <div>
-
-        { this.props.onLandingPage ? <Link to="profile">Profile</Link> :
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="text">TextView</Link>
-          <Link to="speech">Speech</Link>
-          <Link to="profile">Profile</Link>
-        </div>
+        {
+          this.props.onLandingPage ? <Link to="profile">Profile</Link> :
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="text">TextView</Link>
+              <Link to="speech">Speech</Link>
+              <Link to="profile">Profile</Link>
+            </div>
         }
       </div>
     );
