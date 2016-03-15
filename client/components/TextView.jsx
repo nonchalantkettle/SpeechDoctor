517a5be6da2c5e3971ab857fad5bcb992bdc86ac
Anthony Salierno <anthony.salierno@gmail.com>
2016-03-15 15:49:00 -0700

    Implement state changes on analyze button click and on typing in InputForm

--- client/components/TextView.jsx
+++ client/components/TextView.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
 import InputForm from './InputForm.jsx';
+import TextAnalytics from './TextAnalytics.jsx';
 
 export default class TextView extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      showAnalytics: false,
+      visibleAnalytics: false,
+      value: ''
     };
   }
 
-  handleClick() {
+  analyzeText = () => {
     this.setState({
-      showAnalytics: true,
+      visibleAnalytics: true,
+    });
+  }
+
+  resetText = () => {
+    this.setState({
+      visibleAnalytics: false,
     });
   }
 
   render() {
+    let analytics = this.state.visibleAnalytics ? <TextAnalytics /> : "";
     return (
       <div>
         <h1>Text Analyzer</h1>
-         <InputForm />
-           <button>
-            Analyze Text
-           </button>
-         {/*
-          TextAnalytics component below "Analyze Button"
-        */}
-        
+         <InputForm value={this.state.value}/>
+           <button onClick={this.analyzeText}>Analyze</button>
+           <button onClick={this.resetText}>Reset</button>
+           <div>{this.state.value}</div>
+           <div>{analytics}</div>
       </div>
     );
   }
