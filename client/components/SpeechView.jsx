3391a9ac2370bd7c2677ef4e5d7a0a950b05773f
Anthony Salierno <anthony.salierno@gmail.com>
2016-03-18 11:22:16 -0700

    Render basic SpeechView and RecordingView components with no functionality

--- client/components/SpeechView.jsx
+++ client/components/SpeechView.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
+import RecordingView from './RecordingView.jsx';
+import SpeechAnalytics from './SpeechAnalytics.jsx';
 
 export default class SpeechView extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      showText: false,
+      visibleAnalytics: false,
+      value: '',
     };
   }
 
-  handleClick() {
+  analyzeText = () => {
     this.setState({
-      showText: true,
+      visibleAnalytics: true,
     });
+  }
+
+  resetText = () => {
+    this.setState({
+      visibleAnalytics: false,
+      value: '',
+    });
+  }
+
+  handleChange = (e) => {
+    this.state.value = e.target.value;
   }
 
   render() {
     return (
-      <h1>Speech View!</h1>
+
+      <div>
+        <div id='speech-input'>
+          <h1 id='speech-input-title'>Speech Analyzer</h1>
+            <RecordingView />
+        </div>
+      </div>
     );
   }
 }
