c3d3f86bf46e535fcf153b0cd565d2e6b4c8abe4
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-19 15:58:31 -0700

    Solve merge conflicts

--- client/components/RecordingView.jsx
+++ client/components/RecordingView.jsx
@@ -1,42 +1,72 @@
 import React from 'react';
+import SpeechAnalytics from './SpeechAnalytics.jsx';
 
 export default class RecordingView extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      recording: false,
-      results: '',
-    };
-  }
+ constructor(props) {
+   super(props);
+   this.state = {
+     recording: false,
+     results: '',
+     showTranscript: false,
+   };
+ }
 
-  handleClick() {
-    this.setState({
-      recording: !this.state.recording,
-    });
-  }
+ handleClick() {
+   this.setState({
+     recording: !this.state.recording,
+   });
+ }
 
-  listener = () => {
-    this.handleClick();
-    console.log("state", this.state.recording);
-    const recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.onresult = (event) => {
-      this.setState({
-        results: event.results[0][0].transcript,
-      });
-      console.log(event.results[0][0].transcript);
-    }
-    recognition.start();
-  }
+ displayTranscript() {
+   this.setState({
+     showTranscript: !this.state.showTranscript,
+   });
+ }
 
-  render() {
-    return (
-      <div>
-        <button id='speech-record-button' onClick={this.listener}>Click to Record Speech</button>
-        <div id='rendered-speech'>{this.state.results}</div>
-      </div>
-    );
-  }
+ listener = () => {
+   //we might want to remove this, in place for recognition.onstart/onend
+   this.handleClick();
+   const recognition = new webkitSpeechRecognition();
+   recognition.continuous = true;
+   recognition.interimResults = true;
+
+   recognition.onstart = () =>{
+      recognizing: this.state.recording,
+   }
+
+   recognition.onend = () =>{
+      recognizing: this.state.recording,
+   }
+
+   recognition.onresult = (event) => {
+     this.setState({
+       results: event.results[0][0].transcript,
+     });
+   }
+   recognition.start();
+ }
+
+ render() {
+   //let transcript = this.state.showTranscript ? <div>{this.state.showTranscript}</div> : <div></div>
+   let currentState = this.state.recording ? <div>Recording...</div> : <div>Star recording now</div>
+   return (
+     <div>
+       <div id='speech-input'>
+         <div id="recording-view">
+           <button className="record-button" onClick={this.listener}>
+             <img id="record-img" src="assets/record.png" alt="record"/>
+           </button>
+           <img id="record-img" src="assets/play.png" alt="play"/>
+           <img id="record-img" src="assets/pause.png" alt="pause"/>
+         </div>
+       </div>
+       <span>{currentState}</span>
+       {/*Make this button toggle between Record and Stop Recording ALSO WHAT IS ALT*/}
+       <div>
+         <div id='rendered-speech'>{this.state.results}</div>
+       </div>
+     </div>
+   );
+ }
 }
