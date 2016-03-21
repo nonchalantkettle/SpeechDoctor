7a5117b525d20a79ff61968fb827a8041e13d0e0
Benny Hung <bennyhungry@gmail.com>
2016-03-21 15:26:49 -0700

    Address linter errors in LandingPage.jsx

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -1,37 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 import LandingButton from './LandingButton.jsx';
 
-export default class LandingPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showText: false,
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>SpeechDoctor</h1>
-        <div id="landing-buttons">
-          <div id="left-landing">
-            <LandingButton
-              handleLandingBtnClick={this.props.handleLandingBtnClick}
-              directTo={'text'}
-              buttonName={'Click to analyze text'}
-            />
-          </div>
-          <div id="right-landing">
-            <LandingButton
-              handleLandingBtnClick={this.props.handleLandingBtnClick}
-              directTo={'speech'}
-              buttonName={'Click to analyze speech'}
-            />
-          </div>
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>SpeechDoctor</h1>
+      <div id="landing-buttons">
+        <div id="left-landing">
+          <LandingButton
+            directTo={'text'}
+            buttonName={'Click to analyze text'}
+          />
+        </div>
+        <div id="right-landing">
+          <LandingButton
+            directTo={'speech'}
+            buttonName={'Click to analyze speech'}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
