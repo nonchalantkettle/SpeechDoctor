e7c1ae78e1d9bd5553e4a924303a3c293e8a9b02
Benny Hung <bennyhungry@gmail.com>
2016-03-21 15:31:24 -0700

    Address linter errors in Nav.jsx

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default class Nav extends React.Component {
-  render() {
-    return (
-      <div id="navbar">
-        {
-          this.props.onLandingPage ? <div><div id="navlink"><Link to="profile">Your Profile</Link></div></div> :
-            <div>
-              <div id="navlink"><Link onClick={this.props.handleHomeClick}to="/">Home</Link></div>
-              <div id="navlink"><Link to="text">Upload Text</Link></div>
-              <div id="navlink"><Link to="speech">Record Speech</Link></div>
-              <div id="navlink"><Link to="profile">Your Profile</Link></div>
-            </div>
-        }
-      </div>
-    );
-  }
+export default function Nav(prop) {
+  return (
+    <div id="navbar">
+      <div id="navlink"><Link onClick={prop.handleHomeClick}to="/">Home</Link></div>
+      <div id="navlink"><Link to="text">Upload Text</Link></div>
+      <div id="navlink"><Link to="speech">Record Speech</Link></div>
+      <div id="navlink"><Link to="profile">Your Profile</Link></div>
+    </div>
+  );
 }
