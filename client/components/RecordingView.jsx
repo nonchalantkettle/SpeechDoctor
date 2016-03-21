f8dab68d31a8596bfc971f5b2d3cb78d6447c769
Benny Hung <bennyhungry@gmail.com>
2016-03-21 11:02:32 -0700

    Fix indentation issues

--- client/components/RecordingView.jsx
+++ client/components/RecordingView.jsx
@@ -4,56 +4,56 @@ import SpeechAnalytics from './SpeechAnalytics.jsx';
 export default class RecordingView extends React.Component {
 
  constructor(props) {
-   super(props);
-   this.state = {
-     recording: false,
-     results: '',
-     showTranscript: false,
-     clearHearing: true,
-   };
- }
+    super(props);
+    this.state = {
+      recording: false,
+      results: '',
+      showTranscript: false,
+      clearHearing: true,
+    };
+  }
 
- handleClick() {
-   this.setState({
-     recording: !this.state.recording,
-   });
- }
+  handleClick() {
+    this.setState({
+      recording: !this.state.recording,
+    });
+  }
 
  displayTranscript() {
-   this.setState({
-     showTranscript: !this.state.showTranscript,
-   });
- }
+    this.setState({
+      showTranscript: !this.state.showTranscript,
+    });
+  }
 
- listener = () => {
-   //we might want to remove this, in place for recognition.onstart/onend
-   //Right now, it will stop recording when the user stops speaking for a few seconds
-   const recognition = new webkitSpeechRecognition();
-   recognition.continuous = true;
-   recognition.interimResults = true;
+  listener = () => {
+    //we might want to remove this, in place for recognition.onstart/onend
+    //Right now, it will stop recording when the user stops speaking for a few seconds
+    const recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
 
-   recognition.onresume = () => {
-     console.log("continuting")
-   }
+    recognition.onresume = () => {
+      console.log("continuting")
+    }
 
-   recognition.onend = () => {
-     recognition.start()
-   }
+    recognition.onend = () => {
+      recognition.start()
+    }
 
-   recognition.onresult = (event) => {
-     console.log(event.results)
-     let returnedTranscript = "";
-     let threshold = 0.75;
-     let confidence = true;
+    recognition.onresult = (event) => {
+      console.log(event.results)
+      let returnedTranscript = "";
+      let threshold = 0.75;
+      let confidence = true;
 
-     for(let i = 0; i < event.results.length; i++){
-       if(event.results[i][0].confidence  < threshold){
-         confidence = false;
-       } else {
-         confidence = true;
-       }
-       returnedTranscript += event.results[i][0].transcript
-     }
+      for(let i = 0; i < event.results.length; i++){
+        if(event.results[i][0].confidence  < threshold){
+          confidence = false;
+        } else {
+          confidence = true;
+        }
+        returnedTranscript += event.results[i][0].transcript
+      }
 
      this.setState({
        results: returnedTranscript,
@@ -65,30 +65,30 @@ export default class RecordingView extends React.Component {
 
  }
 
- render() {
-   let hearingClearly = this.state.clearHearing ?  <div></div> :
-   <div>Sorry, we are having trouble understanding you
-   Please speek more clearly</div>
+  render() {
+    let hearingClearly = this.state.clearHearing ?  <div></div> :
+    <div>Sorry, we are having trouble understanding you
+    Please speek more clearly</div>
 
-   let currentState = this.state.recording ? <div>Recording...</div> : <div>Star recording now</div>
-   return (
-     <div>
-       <div id='speech-input'>
-         <div id="recording-view">
-           <button className="record-button" onClick={this.listener}>
-             <img id="record-img" src="assets/record.png" alt="record"/>
-           </button>
-           <img id="record-img" src="assets/play.png" alt="play"/>
-           <img id="record-img" src="assets/pause.png" alt="pause"/>
-         </div>
-       </div>
-       <span>{currentState}</span>
-       <span>{hearingClearly}</span>
-       {/*Make this button toggle between Record and Stop Recording ALSO WHAT IS ALT*/}
-       <div>
-         <div id='rendered-speech'>{this.state.results}</div>
-       </div>
-     </div>
-   );
- }
+    let currentState = this.state.recording ? <div>Recording...</div> : <div>Star recording now</div>
+    return (
+      <div>
+        <div id='speech-input'>
+          <div id="recording-view">
+            <button className="record-button" onClick={this.listener}>
+              <img id="record-img" src="assets/record.png" alt="record"/>
+            </button>
+            <img id="record-img" src="assets/play.png" alt="play"/>
+            <img id="record-img" src="assets/pause.png" alt="pause"/>
+          </div>
+        </div>
+        <span>{currentState}</span>
+        <span>{hearingClearly}</span>
+        {/*Make this button toggle between Record and Stop Recording ALSO WHAT IS ALT*/}
+        <div>
+          <div id='rendered-speech'>{this.state.results}</div>
+        </div>
+      </div>
+    );
+  }
 }
