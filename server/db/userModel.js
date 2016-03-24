95f6c28f9efa971f0428b1474970f5d43959deae
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2016-03-24 14:51:35 -0700

    Continue working on user model - add some auth methods

--- server/db/userModel.js
+++ server/db/userModel.js
@@ -1,14 +1,54 @@
-// const mongoose = require('mongoose');
+/* eslint new-cap: 0 */
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const Q = require('q');
 
-// const UserSchema = new mongoose.Schema({
-//   username: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//   },
-//   salt: String,
-// });
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  salt: String,
+});
+
+UserSchema.methods.comparePasswords = (candidatePassword) => {
+  const savedPassword = this.password;
+  return Q.Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, savedPassword, (err, matched) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(matched);
+      }
+    });
+  });
+};
+
+UserSchema.pre('save', (next) => {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  return bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    return bcrypt.hash(user.password, salt, null, (err2, hash) => {
+      if (err2) {
+        return next(err2);
+      }
+      user.password = hash;
+      user.salt = salt;
+      return next();
+    });
+  });
+});
+
+module.exports = mongoose.model('User', UserSchema);
