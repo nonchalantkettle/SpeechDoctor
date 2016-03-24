550f029d43cc2e832b8650d0cef095117f6c23d3
Alexxis Johnson <alexxis.r.p.johnson@gmail.com>
2016-03-23 17:14:32 -0700

    Display new analytics and edit formatting of old analytics

--- client/components/TextAnalytics.jsx
+++ client/components/TextAnalytics.jsx
@@ -1,8 +1,14 @@
+/* eslint no-param-reassign: 0*/
 import React from 'react';
 import $ from 'jquery';
-import { getDefs, getSyns, analyzeText } from '../../server/utils/customTextAnalytics.js';
+import { getDefs,
+         getSyns,
+         analyzeText,
+         getTextStats,
+         getAutomatedReadabilityIndex }
+         from '../../server/utils/customTextAnalytics.js';
 
-function renderAnalytics(string) {
+function renderTopThree(string) {
   const analyticsObj = analyzeText(string);
   // const countEachWordResult = analyticsObj.allTotals;
   const topThreeWordsResult = analyticsObj.topThree;
@@ -17,7 +23,9 @@ function renderAnalytics(string) {
 
 export default function TextAnalytics(prop) {
   if (prop.text) {
-    renderAnalytics(prop.text).map((word) =>
+    const counts = getTextStats(prop.text);
+    const ARI = getAutomatedReadabilityIndex(prop.text);
+    renderTopThree(prop.text).map((word) =>
       getDefs(word[0], (defErr, defData) => {
         if (defErr) {
           return defErr;
@@ -28,19 +36,41 @@ export default function TextAnalytics(prop) {
             return synErr;
           }
           const synonyms = synData.syns;
-          $('#topThreeMostUsed').append(`<div id=${word[0]}>${word}
-            <div id="partOfSpeech">Part of Speech: ${defintionAndPos.pos}</div>
-            <div id="definition">Definition: ${defintionAndPos.def}</div>
+
+          $('#topThreeMostUsed').append(`<div id=${word[0]}><p>${word[0]}${word[1]}</p>
+            <p id="partOfSpeech">Part of Speech: ${defintionAndPos.pos}</div>
+            <p id="definition">Definition: ${defintionAndPos.def}</div>
           </div>`);
-          synonyms.map((syn) => $('#topThreeMostUsed').append(`<li>${syn.word}</li>`));
-          return $('#topThreeMostUsed').append('<hr />');
+
+          if (synonyms.length) {
+            let synString = synonyms.reduce((acc, syn) => {
+              acc += `${syn.word}, `;
+              return acc;
+            }, '');
+            synString = synString.slice(0, -2);
+            $('#topThreeMostUsed').append(`<p>Synonyms: ${synString}</p>`);
+          }
+
+          return synonyms;
         });
       })
     );
     return (
       <div>
-        <p>Here are your results:</p>
-        <p id="topThreeMostUsed">Top Three Most Used Words: </p>
+        <h2>Results</h2>
+        <h3>General Text Stats</h3>
+        <div>
+          <p>Total Characters (all): {counts.charsWithSpace}</p>
+          <p>Total Characters (no spaces): {counts.charsNoSpace}</p>
+          <p>Total Characters (no punctuation or spaces): {counts.charsJustLetters}</p>
+          <p>Total Words: {counts.words}</p>
+          <p>Total Sentences: {counts.sentences}</p>
+          <p>Total Paragraphs: {counts.paragraphs}</p>
+          <p>Average Characters Per Word: {counts.charactersPerWord}</p>
+          <p>Average Words Per Sentence: {counts.wordsPerSentence}</p>
+          <p>Automated Readability Index: Target Age - {ARI.age} Target Grade {ARI.grade}</p>
+        </div>
+        <h3 id="topThreeMostUsed">Most-Used Words</h3>
       </div>
     );
   }
