66aee6c1dd72dea2928259d4cc8c72bb779c6092
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-25 12:01:19 -0700

    Implement timer methods in textView

--- client/components/TextView.jsx
+++ client/components/TextView.jsx
@@ -3,6 +3,9 @@ import $ from 'jquery';
 import InputForm from './InputForm.jsx';
 import TextAnalytics from './TextAnalytics.jsx';
 import WordCloud from './WordCloud.jsx';
+import Timer from './Timer.jsx';
+
+let WPM = 0;
 
 export default class TextView extends React.Component {
 
@@ -11,7 +14,23 @@ export default class TextView extends React.Component {
     this.state = {
       visibleAnalytics: false,
       value: '',
+      secondsElapsed: 0,
     };
+  }
+
+  getMinutes() {
+    return Math.floor(this.state.secondsElapsed / 60);
+  }
+
+  getSeconds() {
+    return Math.floor(this.state.secondsElapsed % 60);
+  }
+
+  resetText() {
+    this.setState({
+      visibleAnalytics: false,
+      value: '',
+    });
   }
 
   analyzeText(input) {
@@ -21,12 +40,28 @@ export default class TextView extends React.Component {
     });
   }
 
-  resetText() {
+  startTimer() {
+    this.incrementer = setInterval(() => {
+      this.setState({
+        secondsElapsed: (this.state.secondsElapsed + 1),
+      });
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.incrementer);
     this.setState({
-      visibleAnalytics: false,
-      value: '',
+      secondsElapsed: 0,
     });
   }
+
+  calculateWPM() {
+    const numberOfWords = this.state.results.split(' ').length;
+    const time = this.state.secondsElapsed;
+    const wordsPerSecond = (numberOfWords / time);
+    WPM = Math.floor(wordsPerSecond * 60);
+  }
+
 
   render() {
     const removeWordCloud = () => {
@@ -37,6 +72,7 @@ export default class TextView extends React.Component {
 
     const analytics = this.state.visibleAnalytics ?
       <div>
+        You write at {WPM} words per minute.
         <TextAnalytics text={this.state.value} />
         <WordCloud text={this.state.value} />
       </div>
@@ -47,11 +83,18 @@ export default class TextView extends React.Component {
       resetText: this.resetText.bind(this),
     };
 
+    const timerMethods = {
+      getSeconds: this.getSeconds.bind(this),
+      getMinutes: this.getMinutes.bind(this),
+      secondsElapsed: this.state.secondsElapsed,
+    };
+
     return (
       <div>
         <div id="text-input">
           <h1 id="text-input-title">Text Analyzer</h1>
           <InputForm {...inputFormMethods} />
+          <Timer {...timerMethods} />
           {analytics}
         </div>
       </div>
