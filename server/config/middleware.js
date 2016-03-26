b55738f4784124b3ef3375797383ef539d68b975
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-26 15:27:29 -0700

    Add middleware to handle cross-origin requests

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -30,6 +30,7 @@ module.exports = (app, express) => {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin: http://localhost:8080');
     next();
   });
 };
