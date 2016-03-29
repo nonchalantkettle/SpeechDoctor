437142d47b1df1bdb3a1d12c66706068d8171093
Benny Hung <bennyhungry@gmail.com>
2016-03-29 11:00:36 -0700

    Clean up comments

--- server/utils/customTextAnalytics.js
+++ server/utils/customTextAnalytics.js
@@ -4,7 +4,6 @@ import { wordsAPIKey } from '../../API_KEYS';
  * Helper functions for custom text analytics
  */
 
-// get each word's word count
 export function countEachWord(textInput) {
   // split up input string into arrays by spaces and newline chars
   const whiteSpaceChars = /\s/;
@@ -27,7 +26,6 @@ export function countEachWord(textInput) {
   return wordCountObject;
 }
 
-// checks to see if any 'avoid' words (words the user wants to avoid) were used
 export function checkWordsToAvoid(wordsToAvoidArr, textInput) {
   const allWords = countEachWord(textInput);
   const wordsUsed = {};
@@ -41,9 +39,7 @@ export function checkWordsToAvoid(wordsToAvoidArr, textInput) {
   return wordsUsed;
 }
 
-// find the top three most-used words, excluding 'the', 'a', 'an', and 'and'
 function topThreeWords(wordCountObject) {
-  // const wordsToIgnore = /the\b|a\b|an\b|and\b|is\b|that\b|to\b|i\b/;
   const wordsToIgnore = /\b[a-z]{1,2}\b|the\b|and\b|that\b|are\b/gi;
 
   // avoid modifying original wordCountObject
@@ -140,18 +136,15 @@ export function getSyns(word, callback) {
 export function analyzeText(userTextInput, wordsToAvoid) {
   const analytics = {};
 
-  // word totals
   const totalOfEachWord = countEachWord(userTextInput);
   analytics.allTotals = totalOfEachWord;
 
-  // words to avoid
   if (wordsToAvoid) {
     analytics.wordsNotAvoided = checkWordsToAvoid(wordsToAvoid, totalOfEachWord);
   } else {
     analytics.wordsNotAvoided = 'No words to avoid';
   }
 
-  // top used words
   const threeMostUsed = topThreeWords(totalOfEachWord);
   analytics.topThree = threeMostUsed;
 
