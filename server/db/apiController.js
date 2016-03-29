3ed677403d68709166fc4651e0556e11792b8584
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-29 12:15:29 -0700

    Add functionality and fix linter errors

--- server/db/apiController.js
+++ server/db/apiController.js
@@ -1,29 +1,29 @@
-const Q = require('q');
-const jwt = require('jsonwebtoken');
-const api  = require('../../API_KEYS');
-const httpRequest = require('http-request')
+const api = require('../../API_KEYS');
+const httpRequest = require('http-request');
 const parseString = require('xml2js').parseString;
 
 module.exports = {
-  dictionary: (req, res, next) => {
+  dictionary: (req, res) => {
     const reqWord = req.params.word;
     const webster = `http://www.dictionaryapi.com/api/v1/references/collegiate/xml/${reqWord}?key=${api.websterDictionaryAPI}`;
-    httpRequest.get(webster, function (err, data) {
+    httpRequest.get(webster, (err, data) => {
       if (err) {
-        console.error("THERE WAS AN ERROR : ", err);
         return;
       }
-      parseString(data.buffer, function(err, result){
-        var dictionaryEntries = result.entry_list.entry[0].def[0].dt;
-        var def;
-        for(var i = 0 ; i<dictionaryEntries.length ; i++){
-          if(typeof dictionaryEntries[i] === 'string'){
+      parseString(data.buffer, (error, result) => {
+        if (error) {
+          res.send(error);
+          return;
+        }
+        const dictionaryEntries = result.entry_list.entry[0].def[0].dt;
+        for (let i = 0; i < dictionaryEntries.length; i ++) {
+          if (typeof dictionaryEntries[i] === 'string') {
             res.send(dictionaryEntries[i]);
             return;
           }
         }
-        for(var j = 0 ; j<dictionaryEntries.length ; j++){
-          if(dictionaryEntries[j]._.length > 2){
+        for (let j = 0; j < dictionaryEntries.length; j ++) {
+          if (dictionaryEntries[j]._.length > 2) {
             res.send(dictionaryEntries[j]._);
             return;
           }
@@ -32,21 +32,26 @@ module.exports = {
     });
   },
 
-  thesaurus: (req, res, next) => {
+  thesaurus: (req, res) => {
     const reqWord = req.params.word;
     const webster = `http://www.dictionaryapi.com/api/v1/references/thesaurus/xml/${reqWord}?key=${api.websterThesaurusAPI}`;
-    httpRequest.get(webster, function (err, data) {
+    httpRequest.get(webster, (err, data) => {
       if (err) {
         res.send(err);
+        return;
       }
-      parseString(data.buffer, function(err, result){
-        var thesaurusEntries = result.entry_list.entry;
-        var thesaurusObj = {
+      parseString(data.buffer, (error, result) => {
+        if (error) {
+          res.send(error);
+          return;
+        }
+        const thesaurusEntries = result.entry_list.entry;
+        const thesaurusObj = {
           pos: thesaurusEntries[0].fl[0],
           syns: thesaurusEntries[0].sens[0].syn[0],
         };
         res.send(thesaurusObj);
       });
-    })
-  }
-}
+    });
+  },
+};
