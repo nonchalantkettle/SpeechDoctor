6fd623cad42c139e18d74e3bad6ecbd1f539de48
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-30 15:41:35 -0700

    Begin adding api calls to speech analytics

--- client/components/Speech/SpeechAnalytics.jsx
+++ client/components/Speech/SpeechAnalytics.jsx
@@ -3,10 +3,8 @@
 import React from 'react';
 import { Link } from 'react-router';
 import $ from 'jquery';
-
-import { getDefs,
-         getSyns,
-         analyzeText,
+import api from '../utils/api';
+import { analyzeText,
          getTextStats,
          getAutomatedReadabilityIndex,
        } from '../../../server/utils/customTextAnalytics.js';
@@ -24,42 +22,36 @@ function renderTopThree(speech) {
 }
 
 export default function SpeechAnalytics(prop) {
-  const askToSave = !prop.userLoggedIn ?
-    <p><Link to="signup">Sign up </Link>or <Link to="login">log in </Link>to save your results</p>
-    : <div></div>;
   if (prop.speech) {
     const counts = getTextStats(prop.speech);
     const ARI = getAutomatedReadabilityIndex(prop.speech);
     renderTopThree(prop.speech).map((word) =>
-      getDefs(word[0], (defErr, defData) => {
+      api.getDefs(word[0], (defErr, defData) => {
         if (defErr) {
           return defErr;
         }
-        const defintionAndPos = defData;
-        return getSyns(word[0], (synErr, synData) => {
+
+        return api.getSyns(word[0], (synErr, synData) => {
           if (synErr) {
             return synErr;
           }
-          const synonyms = synData.syns;
+
+          const syns = synData.syns;
+          const pos = synData.pos;
+          const def = defData;
+
           $('#topThreeMostUsed').append(`<p id="bold-word">${word[0]}${word[1]}</p>
-            <p>Part of Speech: ${defintionAndPos.pos}</p>
-            <p>Definition: ${defintionAndPos.def}</p>`);
-          if (synonyms.length) {
-            let synString = synonyms.reduce((acc, syn) => {
-              acc += `${syn.word}, `;
-              return acc;
-            }, '');
-            synString = synString.slice(0, -2);
-            $('#topThreeMostUsed').append(`<p>Synonyms: ${synString}</p>`);
-          }
-          return synonyms;
+            <p>Part of Speech: ${pos}</p>
+            <p>Definition: ${def}</p>
+            <p>Synonyms: ${syns}</p>`);
+
+          return syns;
         });
       })
     );
     return (
       <div>
         <h2>Results</h2>
-        {askToSave}
         <h3>General Speech Stats</h3>
         <div>
           <p>Total Characters (all): <span id="bold-word">{counts.charsWithSpace}</span></p>
