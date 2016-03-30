6044aa8b50831092adb0fbfb71b4f7d5d52d4591
Benny Hung <bennyhungry@gmail.com>
2016-03-29 22:00:25 -0700

    GET all text data before mounting user profile. No longer need button to analyze text

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -11,48 +11,26 @@ export default class UserProfile extends React.Component {
     };
   }
 
-  handleTextClick() {
+  componentWillMount() {
     $.get('/text', { username: this.props.user })
-      .done((data) => {
-        this.setState({
-          showAnalytics: true,
-          data,
-        });
-      })
-      .fail((err) => {
-        throw new Error('Could not retrieve text information', err);
+    .done((data) => {
+      this.setState({
+        showAnalytics: true,
+        data,
       });
+    })
+    .fail((err) => {
+      throw new Error('Could not retrieve text information', err);
+    });
   }
 
-  // handleSpeechClick() {
-  //   $.get('/speech', { username: this.props.user })
-  //     .done((data) => {
-  //       this.setState({
-  //         showAnalytics: true,
-  //         data: data,
-  //       });
-  //     })
-  //     .fail((err) => {
-  //       throw new Error('Could not retrieve text information', err);
-  //     });
-  // }
-
   render() {
-    const onTextClick = this.handleTextClick.bind(this);
-    // const onSpeechClick = this.handleSpeechClick.bind(this);
-
     return (
-      <div>
-        <button onClick={onTextClick}>Analytics</button>
-        {
-          this.state.showAnalytics ?
-          <UserAnalytics data={this.state.data} /> :
-          <div></div>
-        }
+      <div id="analytics-container">
+        {this.state.showAnalytics ? <UserAnalytics data={this.state.data} /> : <div></div>}
       </div>
     );
   }
-
 }
 
 UserProfile.propTypes = {
